56c1b130547cdd30920dc6616ac72d7d3d1c1027
27111984 <colasurdosalvatore@googlemail.com>
2019-06-20 13:43:41 +0200

    salvatest

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
+//   1.Write a program to display the city name if the string begins with "Los" or "New" otherwise return "The city name does not begin with Los or New".
 
+// function city_name(str) {
+//     if (str.length >= 3 && ((str.substring(0, 3) == 'Los')
+//                             || (str.substring(0, 3) == 'New')))
+
+//         {
+//             return str;
+//       }
+
+//     return 'The city name does not begin with Los or New';
+//   }
+
+// 2. Write a program to calculate the sum of three elements of a given array of integers. The length of the array must be 3.
+// Example [5, 10, 15] ➞ 30
+
+// function sum_three(nums)
+// {
+//   return nums[0] + nums[1] + nums[2];
+// }
+
+// 3. Given two strings, firstName and lastName, return a single string in the format "last, first". Examples "John", "Doe" ➞ "Doe, John" "Mary", "Green" ➞ "Green, Mary"
+
+// function concatName(firstName, lastName) {
+// 	return `${lastName}, ${firstName}`;
+// }
+
+// 4.Write a program that checks whether or not an integer is divisible by 100. Examples & Expected Output 1 ➞ false 1000 ➞ true 100 ➞ true
+// const checkNumber = (x) => {
+//     if(number%100 == 0){
+
+//       console.log('true');
+
+//     }else{
+
+//       console.log('false');
+
+//     }
+//   }
+
+// 5. Write a program that adds a string ending to each member in an array. Examples & Expected output ["clever", "meek", "hurried", "nice"], "ly" ➞ ["cleverly", "meekly", "hurriedly", "nicely"]
+
+// ["new", "pander", "scoop"], "er" ➞ ["newer", "panderer", "scooper"]
+
+// ["bend", "sharpen", "mean"], "ing" ➞ ["bending", "sharpening", "meaning"]
